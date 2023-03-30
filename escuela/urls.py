bb93dc31b460b82d1b8a99e2dee080431419c467
unknown <acarles@jaumebalmes.net>
2023-03-30 20:45:13 +0200

    jueves

--- escuela/urls.py
+++ escuela/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index' ),
     path('profesores/', views.profes, name='profesores' ),
+    path('profesores/<str:nombre>', views.profes2, name='profesores' ),
     path('alumnos/', views.alumns, name='alumnos' ),
+    path('alumnos/<str:nombre>', views.alumns2, name='alumnos' ),
+    
 ]
--- escuela/views.py
+++ escuela/views.py
@@ -1,19 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader, Context
-template = loader.get_template('index.html')
 
 def index(request):
-    return HttpResponse(template.render())
+    return render(request,'index.html')
 
 def alumns(request):
+    
     context = {
         "data" : [{"name":"Moises", "surname":"Chukiruna",},{"name":"Marc", "surname":"Soler",},{"name":"Marc", "surname":"Salas",}],
     }
-    return HttpResponse(template.render(context, request))
+    return render(request,'alumnes.html',context)
+
+def alumns2(request, nombre):
+    one = {"data":[]}
+    context = {"data":[{"name":"Moises", "surname":"Chukiruna",},{"name":"Marc", "surname":"Soler",},{"name":"Marco", "surname":"Salas",}]}
+    
+    for i in context["data"]:
+       if i["name"] == nombre:
+        one["data"].append(i)
+
+    return render(request,'alumnes.html',one)
 
 def profes(request):
     context = {
         "data" : [{"name":"Roger", "surname":"Sobrino",},{"name":"Juan", "surname":"Dolores",},{"name":"Pepe", "surname":"Montoya",}],
     }
-    return HttpResponse(template.render(context))
+    return render(request,'profes.html',context)
+
+def profes2(request, nombre):
+    one = {"data":[]}
+    context = {"data"[{"name":"Roger", "surname":"Sobrino",},{"name":"Juan", "surname":"Dolores",},{"name":"Pepe", "surname":"Montoya",}]}
+    
+    for i in context["data"]:
+       if i["name"] == nombre:
+        one["data"].append(i)
+
+    return render(request,'profes.html',one)
