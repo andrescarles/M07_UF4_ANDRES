dea3922a2e75dd59f1c6f9978a9cbea313ad2a5d
unknown <acarles@jaumebalmes.net>
2023-04-06 17:37:01 +0200

    practica17

--- escuela/models.py
+++ escuela/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class tabla(models.Model):
+    campo1 = models.CharField(max_length=30)
+    campo2 = models.CharField(max_length=30)
+    campo3 = models.CharField(max_length=30)
+    campo4 = models.CharField(max_length=30)
+    campo5 = models.CharField(max_length=30)
+    campo6 = models.CharField(max_length=30)
